aafc9695658fa3cb88b8a4a3301efbcbd2696e7c
Kirk Bushell <torm3nt@gmail.com>
2025-03-19 20:12:43 +1100

    Removed the built .js files, which were causing problems for testing.

--- src/output.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    error: (message) => `\x1b[31m${message}\x1b[0m`,
-    info: (message) => `\x1b[36m${message}\x1b[0m`,
-    success: (message) => `\x1b[32m${message}\x1b[0m`,
-};
