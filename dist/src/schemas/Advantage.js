eeadd0c8ea82473ac6dcb4b4da48efac7b00f651
Kirk Bushell <torm3nt@gmail.com>
2025-03-21 23:00:20 +1100

    Fixed the build entry points.

--- dist/src/schemas/Advantage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zod_1 = require("zod");
-exports.default = zod_1.z.object({
-    name: zod_1.z.string(),
-    rules: zod_1.z.string(),
-});
